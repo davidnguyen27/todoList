437794a13613ead379dceea81f24ecf6114ebeb8
TK-Vinh <vinhtkse171805@fpt.edu.vn>
2024-05-19 13:35:51 +0700

    edit-func

--- src/components/body/TableControl.jsx
+++ src/components/body/TableControl.jsx
@@ -1,12 +1,37 @@
-import { Table, Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Table, Form, Button, Modal } from 'react-bootstrap';
 
 const TableControl = (props) => {
   const { todos, setTodos } = props;
+  const [showModal, setShowModal] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState(null);
 
   const handleDelete = (id) => {
     const currentTodo = todos.filter((todo) => todo.id !== id);
     setTodos(currentTodo);
     localStorage.setItem('todos', JSON.stringify(currentTodo));
+  };
+
+  const handleStatusChange = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, status: !todo.status } : todo
+    );
+    setTodos(updatedTodos);
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+  };
+
+  const handleEdit = (todo) => {
+    setCurrentTodo(todo);
+    setShowModal(true);
+  };
+
+  const handleUpdate = () => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === currentTodo.id ? currentTodo : todo
+    );
+    setTodos(updatedTodos);
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+    setShowModal(false);
   };
 
   return (
@@ -31,19 +56,51 @@ const TableControl = (props) => {
                   id={`custom-switch-${todo.id}`}
                   label={todo.status ? 'Finish' : 'Doing'}
                   checked={todo.status}
-                  onChange={() => {}}
+                  onChange={() => handleStatusChange(todo.id)}
                 />
               </td>
               <td>
-                <Button variant="info">Detail</Button>
-                <Button variant="danger" onClick={() => handleDelete(todo.id)}>
-                  Delete
-                </Button>
+                <Button variant="info" onClick={() => handleEdit(todo)}>Edit</Button>
+                <Button variant="danger" onClick={() => handleDelete(todo.id)}>Delete</Button>
               </td>
             </tr>
           ))}
         </tbody>
       </Table>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Todo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formTodoName">
+              <Form.Label>Todo name</Form.Label>
+              <Form.Control
+                type="text"
+                value={currentTodo ? currentTodo.todoName : ''}
+                onChange={(e) => setCurrentTodo({ ...currentTodo, todoName: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group controlId="formCreatedDate">
+              <Form.Label>Created date</Form.Label>
+              <Form.Control
+                type="date"
+                value={currentTodo ? currentTodo.start : ''}
+                onChange={(e) => setCurrentTodo({ ...currentTodo, start: e.target.value })}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleUpdate}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
